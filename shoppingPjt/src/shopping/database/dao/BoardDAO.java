1edd4c27635c98ec6f132656646f443e2d5e4ddb
이재용 <wodyd202@naver.com>
2019-11-26 13:59:04 +0900

    트랜잭션 테스트 완료

--- shoppingPjt/src/shopping/database/dao/BoardDAO.java
+++ shoppingPjt/src/shopping/database/dao/BoardDAO.java
@@ -2,5 +2,6 @@ package shopping.database.dao;
 
 public interface BoardDAO {
 	public String selectNow();
+	public String insert();
 }
 
--- shoppingPjt/test/shopping/test/TestClass.java
+++ shoppingPjt/test/shopping/test/TestClass.java
@@ -14,6 +14,12 @@ public class TestClass {
 
 	@Test
 	public void test() {
-		System.out.println(board.selectNow());
+		try {
+			board.insert();
+			sqlSession.commit();
+		}catch (Exception e) {
+			sqlSession.rollback();
+			e.printStackTrace();
+		}
 	}
 }
